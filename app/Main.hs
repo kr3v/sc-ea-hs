9cbcc60ae9b5de15861c81af156c416eb6e74419
kr3v <d.baynak@protonmail.com>
2023-05-24 00:24:18 +0300

    projectile collision implementation

--- app/Main.hs
+++ app/Main.hs
@@ -5,22 +5,25 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Control.Lens (Lens', makeLenses, over, set, view, (%~), (&), (.~), (^.))
+import Data.Foldable (find)
+import Data.Functor ((<&>))
+import Data.List (unfoldr)
 import Data.List.Index (imap)
 import qualified Data.Map.Strict as Map
+import Data.Maybe (maybeToList)
 import GHC.RTS.Flags (TraceFlags (traceScheduler))
 import Graphics.Gloss.Data.Color (Color, blue, greyN, red, white)
 import Graphics.Gloss.Data.Display (Display (InWindow))
 import Graphics.Gloss.Data.Picture (Picture (..), blank, color, rectangleSolid, scale, text, translate)
 import Graphics.Gloss.Data.Point (Point)
 import Graphics.Gloss.Data.Vector (Vector)
-import Graphics.Gloss.Interface.IO.Game (Event (..), Key (..), KeyState (..), Modifiers, MouseButton (..), SpecialKey (..), playIO)
+import Graphics.Gloss.Interface.IO.Game (Event (..), Key (..), KeyState (..), Modifiers, MouseButton (..), SpecialKey (..), black, playIO)
 import Graphics.Gloss.Interface.Pure.Game (play)
-import Graphics.Gloss.Interface.IO.Game (black)
-import Data.Maybe (maybeToList)
 
 ---
 
@@ -67,7 +70,8 @@ data WorldStatusState = WSS_PLAYER_INPUT | WSS_TURN_IN_PROGRESS deriving (Show)
 data WorldStatus = WorldStatus
   { _turn :: Int,
     _state :: WorldStatusState
-  } deriving (Show)
+  }
+  deriving (Show)
 
 data Surface = Surface
   { _maxHeight :: Int,
@@ -80,7 +84,6 @@ data World = World
   { _surface :: Surface,
     _players :: Map.Map Int Player,
     _projectile :: Maybe Projectile,
-
     _status :: WorldStatus,
     _transformer :: Picture -> Picture
   }
@@ -94,12 +97,14 @@ data PlayerControls = PlayerControls
 data PlayerObject = PlayerObject
   { _pos :: Point,
     _color :: Color
-  } deriving (Show)
+  }
+  deriving (Show)
 
 data Player = Player
   { _object :: PlayerObject,
     _controls :: PlayerControls
-  } deriving (Show)
+  }
+  deriving (Show)
 
 data ProjectileType = SHELL deriving (Show)
 
@@ -107,7 +112,8 @@ data Projectile = Projectile
   { _ppos :: Point,
     _speed :: Vector,
     _ptype :: ProjectileType
-  } deriving (Show)
+  }
+  deriving (Show)
 
 $(makeLenses ''World)
 $(makeLenses ''WorldStatus)
@@ -217,19 +223,34 @@ worldEventHandler e w@(World s ps p st@(WorldStatus p' s') t) =
       EventMotion (x, y) -> return w
       _ -> print e >> return w
 
+produce :: Float -> Float -> [Float]
+produce x1 x2 = concat $ unfoldr (\x -> if x < x2 then Just ([x - 0.01, x + 0.01], x + 1) else Nothing) (fromIntegral $ floor x1 + 1)
+
+iterateProjectile :: Float -> Surface -> Projectile -> (Maybe Point, Projectile)
+iterateProjectile f s (Projectile (x0, y0) (vx, vy) t) =
+  let dx = vx * f
+      dy = vy * f
+      x1 = x0 + dx
+      y1 = y0 + dy
+      p = Projectile (x1, y1) (vx, vy - 10 * f) t
+      xs = produce x0 x1
+      ys = (\x -> (x - x0) / dx * dy + y0) <$> xs
+      heights = (\x -> snd $ putOn s (x, 0)) <$> xs
+      collision = find (uncurry (<)) (zip ys heights)
+   in (collision, p)
+   
+-- over status (set state WSS_PLAYER_INPUT . set turn (if t == 1 then 2 else 1)) w
+handleProjectileCollision :: Point -> World -> World
+handleProjectileCollision c w@(World s ps (Just p) st@(WorldStatus t WSS_TURN_IN_PROGRESS) _) = over status (set state WSS_PLAYER_INPUT . set turn (if t == 1 then 2 else 1)) w
+
 worldTickHandler :: Float -> World -> IO World
-worldTickHandler f w@(World s ps p st@(WorldStatus p' s') t) = do
-  print f
-  case s' of
-    WSS_TURN_IN_PROGRESS -> case p of 
-      (Just p''@(Projectile (x, y) (vx, vy) t)) -> do
-        print p''
-        let x' = x + vx * f
-            y' = y + vy * f
-            p' = Projectile (x', y') (vx, vy - 10 * f) t
-        return $ set projectile (Just p') w
-      Nothing -> return $ over status (set state WSS_PLAYER_INPUT) w
-    WSS_PLAYER_INPUT -> return w
+worldTickHandler f w@(World _ _ _ (WorldStatus _ WSS_PLAYER_INPUT) _) = return w
+worldTickHandler f w@(World _ _ Nothing (WorldStatus _ WSS_TURN_IN_PROGRESS) _) = return $ over status (set state WSS_PLAYER_INPUT) w
+worldTickHandler f w@(World s _ (Just p) st@(WorldStatus _ WSS_TURN_IN_PROGRESS) _) =
+  let (c, p') = iterateProjectile f s p
+   in return $ case c of
+        Just p -> handleProjectileCollision p w
+        Nothing -> set projectile (Just p') w
 
 --
 
@@ -241,9 +262,9 @@ main :: IO ()
 main = do
   let mx = 1000 :: Int
       my = 1000 :: Int
-      emptyControls = PlayerControls (wrap 45) (wrap 50)
+      emptyControls = PlayerControls (wrap 75) (wrap 50)
       windowSize = (mx, my)
-      transformer = transformPicture (-fromIntegral mx / 2,-fromIntegral my / 2)
+      transformer = transformPicture (-fromIntegral mx / 2, -fromIntegral my / 2)
       surface = Surface my mx $ sinSurface mx my <$> take (mx + 1) [0 ..]
       player1 = Player (PlayerObject (putOn surface (250, 0)) red) emptyControls
       player2 = Player (PlayerObject (putOn surface (750, 0)) blue) emptyControls
